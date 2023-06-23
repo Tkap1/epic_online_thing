078e77a9e6e72e68abd1b7054369f96533f5c36a
blowfishfugu <coolm12345@hotmail.de>
2023-06-23 22:11:57 +0200

    flags for poor people having two graphic cards

--- src/window.c
+++ src/window.c
@@ -1,3 +1,10 @@
+//have been shocked, wglGetProc("glBufferData") failed, 
+//then remembered that integrated graphic has incomplete gl-standards
+//for notebooks, choose graphicscard instead of embedded intel-graphics.
+//nv and amd look for theese exported flags
+_declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
+_declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+
 
 #define m_gl_funcs \
 X(PFNGLBUFFERDATAPROC, glBufferData) \
